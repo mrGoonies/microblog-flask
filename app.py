a1ecdc1cf8459b7bc8478ff996677d6cc6c95c01
mrGoonies <gooniesdev@gmail.com>
2023-10-05 13:32:10 -0300

    Completando estilos del Recent y home

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
+entries = list()
 
 
-@app.get("/home")
+@app.post("/")
 @app.get("/")
 def home():
     current_year = datetime.date.today().year
     title = "Home"
-    return render_template("base.html", title=title, year=current_year)
+
+    if request.method == "POST":
+        date_post = datetime.date.today()
+        entries.append(
+            {
+                "entry_title": request.form.get("title"),
+                "entry_content": request.form.get("content"),
+                "date_post": date_post,
+            }
+        )
+
+    print(entries)
+    return render_template("home/home.html", title=title, year=current_year)
+
+
+@app.get("/post")
+def post():
+    title = "posts"
+    return render_template("home/recent.html", title=title, recent=entries)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
