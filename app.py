6aa40efea9831706669ad0523acfd611f1b0130d
mrGoonies <gooniesdev@gmail.com>
2023-10-07 00:56:37 -0300

    Proyecto finalizado

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
 import datetime
+import os
 
 app = Flask(__name__)
-entries = list()
+
+load_dotenv()
+user = os.environ.get("MONGO_USER")
+pwd = os.environ.get("MONGO_PWD")
+client = MongoClient(f"mongodb+srv://{user}:{pwd}@cluster0.4pqdtun.mongodb.net/")
+app.db = client.MicroBlog
 
 
 @app.post("/")
@@ -12,11 +20,15 @@ def home():
     title = "Home"
 
     if request.method == "POST":
+        # Obteniendo valores del formulario
         entry_title = request.form.get("title")
         date_post = datetime.date.today().strftime("%Y-%m-%d")
         entry_content = request.form.get("content")
-        entries.append((entry_title, date_post, entry_content))
-    print(entries)
+
+        # Agregando datos a MongoDB
+        app.db.entries.insert_one(
+            {"title": entry_title, "date": date_post, "content": entry_content}
+        )
 
     return render_template("home/home.html", title=title, year=current_year)
 
@@ -24,7 +36,9 @@ def home():
 @app.get("/post")
 def post():
     title = "posts"
-    return render_template("home/recent.html", title=title, entries=entries)
+    posts = [obj for obj in app.db.entries.find()]
+
+    return render_template("home/recent.html", title=title, entries=posts)
 
 
 if __name__ == "__main__":
